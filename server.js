e21ce57f21eac5ceb394fb01cd33aca8c633ba03
Deaxz <25507425+Deaxz@users.noreply.github.com>
2020-07-07 01:13:37 +0200

    MySQL connection to DB

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const http = require("http");
+const mysql = require('mysql');
+
+// Create connection to DB.
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "cego"
+});
+
+// Testing query.
+let sql = "SELECT * FROM users";
 
 http.createServer((req, res) =>  {
 
-  console.log(`Request received: {method: '${req.method} url: '${req.url}'}`);
+  console.log(`Request received: {method: '${req.method}' url: '${req.url}'}`);
 
-
+  // Connecting to DB.
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Result: " + result);
+    });
+  });
+  
 }).listen(8000);
