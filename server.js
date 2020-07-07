5ea1fe9bd31aae50b752ae6ec00c351b69109475
Deaxz <25507425+Deaxz@users.noreply.github.com>
2020-07-07 21:03:55 +0200

    Added delete query
    Modifies select query to a delete query.

--- server.js
+++ server.js
@@ -24,21 +24,30 @@ http.createServer((req, res) =>  {
   // Parses url and returns query part of url object.
   let queryData = url.parse(req.url, true).query;
   
-  // Assigns the sql string from query object to sql variable.
-  let sql = queryData.sql;
-  
+  // Assigns the sql string from query object to sql variable, and makes it lowercase.
+  let sql = queryData.sql.toLowerCase();
+
   // Select query.
   db.query(sql, function (err, result) {
     if (err) throw err;
-    //console.log("Result: " + JSON.stringify(result));
+    console.log("Result: " + JSON.stringify(result));
 
     // Creating and writing to file. Overwrites if it exists.
-    fs.writeFile('result.txt', JSON.stringify(result), function (err) {
+    fs.writeFileSync('result.txt', JSON.stringify(result), function (err) {
       if (err) throw err;
       console.log('Saved!');
-    });
+    });   
   });
+
+  // Removes "select *" or "select 'column name'" with "delete".
+  let tempArr = sql.split('');
+  tempArr.splice(0, tempArr.indexOf('f'), 'delete ');
+  let rmsql = tempArr.join('');
   
+  // Deletion query
+  db.query(rmsql, function (err, result) {
+    if (err) throw err;
+    console.log("Deleted: " + JSON.stringify(result));
+  });
+
 }).listen(8000);
-
-
