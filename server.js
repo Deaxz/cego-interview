6705f84dfd421bc314079f6e5c9984b40668b32e
Deaxz <25507425+Deaxz@users.noreply.github.com>
2020-07-07 17:13:36 +0200

    SQL query parsed from URL.
    The program now parses sql from a url. This was done with the URL module.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const mysql = require("mysql");
 const fs = require("fs");
+const url = require("url");
 
 // Create connection to DB.
 let db = mysql.createConnection({
@@ -10,20 +11,22 @@ let db = mysql.createConnection({
   database: "cego"
 });
 
-// Connecting to DB.
+// Connects to DB.
 db.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-
 });
-
-// Testing query.
-let sql = "SELECT * FROM users";
 
 http.createServer((req, res) =>  {
 
   console.log(`Request received: {method: '${req.method}' url: '${req.url}'}`);
- 
+
+  // Parses url and returns query part of url object.
+  let queryData = url.parse(req.url, true).query;
+  
+  // Assigns the sql string from query object to sql variable.
+  let sql = queryData.sql;
+  
   // Select query.
   db.query(sql, function (err, result) {
     if (err) throw err;
